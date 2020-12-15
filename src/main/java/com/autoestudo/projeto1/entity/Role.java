965efdfb1509feb8729258019a3f0326cea20108
Sandro Lopes Bianchini <36516423+sandrolb@users.noreply.github.com>
2020-12-14 21:24:42 -0300

    first commit

---
+++ src/main/java/com/autoestudo/projeto1/entity/Role.java
@@ -0,0 +1,28 @@
+package com.autoestudo.projeto1.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
